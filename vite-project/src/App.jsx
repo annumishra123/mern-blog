c7c03516fc206f8f54663816b5537c4fc7cc8b1f
annumishra123 <anurag786332@gmail.com>
2024-09-02 13:52:28 +0530

    cerate pages and routes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Siginin from './pages/Siginin'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <h1>App</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/sign-in' element={<Siginin />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/dasboard' element={<Dashboard />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
  
